ae73e6d44536bddf083da08ded3f8e3b2aaa7231
liuziyuan <liuziyuan.room@gmail.com>
2019-01-16 17:16:34 +0800

    add flask cors

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_script import Manager, prompt_bool, Command
 from flask_migrate import Migrate, MigrateCommand
+from flask_cors import CORS
 from app import App
 from config.initializers.database import db, command
 from app.models import *
@@ -8,9 +9,9 @@ import psycopg2
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1qaz2wsx#EDC@127.0.0.1:5432/postgres'
-
-my_app = App()
-my_app.init_app(app)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+pss_app = App()
+pss_app.init_app(app)
 
 db.init_app(app)
 migrate = Migrate(app, db)
