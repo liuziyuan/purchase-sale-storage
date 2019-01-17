dfac4195bb0b1d1b72dd505b0afc92c78c54b825
liuziyuan <liuziyuan.room@gmail.com>
2019-01-17 15:18:36 +0800

    remove unused

--- config/initializers/db.py
+++ config/initializers/db.py
@@ -1,4 +1,3 @@
-# from flask_script import Manager, Command, prompt_bool
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
@@ -12,28 +11,3 @@ convention = {
 
 metadata = MetaData(naming_convention=convention)
 db = SQLAlchemy(metadata=metadata)
-
-# command = Manager()
-
-
-# class drop(Command):  
-#     'drop database'  
-#     def run(self):  
-#         if prompt_bool("Are you sure you want to lose all your data"):
-#             db.drop_all()
-
-# class reset(Command):
-#     def run(self):  
-#         if prompt_bool("Are you sure you want to lose all your data"):
-#             db.drop_all()
-#             db.create_all()
-
-# class test(Command):  
-#     'test'  
-#     def run(self):  
-#         print('hello, this is test of dbset command moudle')
-
-
-# command.add_command('test', test)
-# command.add_command('reset', reset)
-# command.add_command('drop', drop)
